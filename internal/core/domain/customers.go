8ccd24b35ea72f43696b1a544d3b4a6eaaa6aad1
lavinas <lavinas@me.com>
2021-11-12 19:36:47 -0300

    valu

--- internal/core/domain/customers.go
+++ internal/core/domain/customers.go
@@ -1,13 +1,16 @@
 package domain
 
 import (
-	"errors"
 	"math"
+	"strconv"
 )
 
 const (
 	env_validate_document = "REQUIRE_DOCUMENT"
-	cpf_length        = 11
+	cpf_min_length        = 8
+	cpf_max_length        = 12
+	cnpj_min_length       = 12
+	cnpj_max_length       = 16
 )
 
 // Customer represents basic informations of a Costumer
@@ -46,17 +49,19 @@ func (c Customer) Validate() error {
 }
 
 // Validate Custumer Name
-func (c Customer) ValidateName() error {
-	if c.Name == "" {
-		return errors.New("name is empty")
-	}
-	return nil
+func (c Customer) IsValidName() bool {
+	return c.Name != ""
 }
 
-// Verify if document is a valid brasilian CPF
+// Verify if document is a valid brasilian CPF (private individual document)
 func (c Customer) IsDocumentCPF() bool {
 	// valid is not zero
 	if c.Document == 0 {
+		return false
+	}
+	// valid length
+	len := len(strconv.FormatUint(c.Document, 10))
+	if len < cpf_min_length || len > cpf_max_length {
 		return false
 	}
 	// valid digit 1
@@ -64,7 +69,7 @@ func (c Customer) IsDocumentCPF() bool {
 	dig2 := int(c.Document%10)
 	val1 := 0
 	val2 := 0
-	for i := 3; i <= cpf_length; i++ {
+	for i := 3; i <= len; i++ {
 		x := int(math.Mod(float64(c.Document), math.Pow10(i)) / math.Pow10(i-1))
 		val1 += x * (i - 1)
 		val2 += x * i
@@ -73,4 +78,43 @@ func (c Customer) IsDocumentCPF() bool {
 	val1 = int(math.Mod(float64(val1*10), float64(11)))
 	val2 = int(math.Mod(float64(val2*10), float64(11)))
 	return val1 == dig1 && val2 == dig2
+}
+
+// Verify if document is a valid brasilian CNPJ (legal entity document)
+func (c Customer) IsDocumentCNPJ() bool {
+	// valid is not zero
+	if c.Document == 0 {
+		return false
+	}
+	// valid length
+	len := len(strconv.FormatUint(c.Document, 10))
+	if len < cnpj_min_length || len > cnpj_max_length {
+		return false
+	}
+	// valid digit 1
+	dig1 := int(c.Document%100/10)
+	dig2 := int(c.Document%10)
+	val1 := 0
+	val2 := 0
+	for i := 0; i <= len - 3; i++ {
+		x := int(math.Mod(float64(c.Document), math.Pow10(i+3)) / math.Pow10(i+2))
+		val1 += x * (int(math.Mod(float64(i), float64(8))) + 2)
+		val2 += x * (int(math.Mod(float64(i+1), float64(8))) + 2)
+	}
+	val2 += dig1 * 2
+	val1 = int(math.Mod(float64(val1), float64(11)))
+	val2 = int(math.Mod(float64(val2), float64(11)))
+	if val1 < 2 {
+		val1 = 0
+	}
+	if val2 < 2{
+		val2 = 0
+	}
+	val1 = 11 - val1
+	val2 = 11 - val2
+	return val1 == dig1 && val2 == dig2
+}
+
+func (c Customer) IsValidDocument() bool {
+	return c.IsDocumentCPF() || c.IsDocumentCNPJ()
 }
--- internal/core/domain/custumers_test.go
+++ internal/core/domain/custumers_test.go
@@ -7,7 +7,20 @@ import (
 
 func TestConstant(t *testing.T) {
 	assert.Equal(t, env_validate_document, "REQUIRE_DOCUMENT")
-	assert.Equal(t, cpf_length, 11)
+	assert.Equal(t, 8,  cpf_min_length)
+	assert.Equal(t, 12, cpf_max_length)
+}
+
+func TestIsValidName(t *testing.T){
+	var c = Customer{}
+	x := c.IsValidName()
+	assert.False(t, x)
+	c = Customer{Name: "Test Name"}
+	x = c.IsValidName()
+	assert.True(t, x)
+	c = Customer{Name: ""}
+	x = c.IsValidName()
+	assert.False(t, x)
 }
 
 func TestIsDocumentCPF(t *testing.T) {
@@ -18,6 +31,39 @@ func TestIsDocumentCPF(t *testing.T) {
 	x = c.IsDocumentCPF()
 	assert.False(t, x)
 	c = Customer{Document: 66946202840}
+	x = c.IsDocumentCPF()
+	assert.False(t, x)
+	c = Customer{Document: 1587547007}
+	x = c.IsDocumentCPF()
+	assert.True(t, x)
+}
+
+func TestIsDocumentCNPJ(t *testing.T) {
+	var c = Customer{Document: 11222333000181}
+	x := c.IsDocumentCNPJ()
+	assert.True(t, x)
+	c = Customer{Document: 11222333000171}
+	x = c.IsDocumentCNPJ()
+	assert.False(t, x)
+	c = Customer{Document: 11222333000182}
+	x = c.IsDocumentCNPJ()
+	assert.False(t, x)
+	c = Customer{Document: 74112977000137}
+	x = c.IsDocumentCNPJ()
+	assert.True(t, x)
+}
+
+func TestIsValidDocument (t *testing.T) {
+	var c = Customer{Document: 66946202848}
+	x := c.IsDocumentCPF()
+	assert.True(t, x)
+	c = Customer{Document: 74112977000137}
+	x = c.IsDocumentCNPJ()
+	assert.True(t, x)
+	c = Customer{Document: 11222333000182}
+	x = c.IsDocumentCNPJ()
+	assert.False(t, x)
+	c = Customer{Document: 66946202818}
 	x = c.IsDocumentCPF()
 	assert.False(t, x)
 }
